46317076edffd04d6e1a2ce517d558d384fc6d55
AlabasterRig <utkrist.j@gmail.com>
2025-02-19 07:55:56 +0530

    initialize project structure with Express, MongoDB, and routing for user and subscription management

---
+++ app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import { PORT } from './config/env.js';
+import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.routes.js';
+import subscriptionRouter from './routes/subscription.routes.js';
+import connectToDB from './database/mongoDB.js';
+
+const app = express();
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/subscriptions', subscriptionRouter);
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the subscription tracker API');
+});
+
+app.listen(PORT, async () => {
+    console.log(`Server tracker is running on http://localhost:${PORT}`);
+    await connectToDB();
+})
+
+export default app;
